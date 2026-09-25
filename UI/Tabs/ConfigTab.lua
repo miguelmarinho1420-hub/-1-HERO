544e1037d5969f5d473c54d076b0841f4df12d1f
miguelmarinho1420-hub <miguel.marinho1420@gmail.com>
2026-09-25 10:19:47 -0300

    Update ConfigTab.lua

--- UI/Tabs/ConfigTab.lua
+++ UI/Tabs/ConfigTab.lua
@@ -1,1 +1,35 @@
+-- Tabs/config.lua
+local ConfigTab = {}
 
+function ConfigTab.Init(Window, flags, destroyCallback)
+    local tab = Window:MakeTab({Name = "Config", Icon = "rbxassetid://6023426915"})
+
+    tab:AddToggle({
+        Name = "Anti-AFK",
+        Default = true,
+        Callback = function(Value)
+            flags.AntiAFK = Value
+        end
+    })
+
+    tab:AddButton({
+        Name = "Desligar / Destruir Script (Unload)",
+        Callback = function()
+            -- Desliga todas as flags principais
+            flags.AutoFarm = false
+            flags.AutoBoss = false
+            flags.AutoDungeon = false
+            flags.AutoEgg = false
+            flags.AutoEndless = false
+            flags.AutoRebirth = false
+            flags.AutoClick = false
+            flags.AntiAFK = false
+
+            if destroyCallback then
+                destroyCallback()
+            end
+        end
+    })
+end
+
+return ConfigTab
