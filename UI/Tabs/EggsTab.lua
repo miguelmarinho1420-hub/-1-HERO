43551a804205772418ffc53e5e94bdfd9f72e83a
miguelmarinho1420-hub <miguel.marinho1420@gmail.com>
2026-09-25 10:19:34 -0300

    Update EggsTab.lua

--- UI/Tabs/EggsTab.lua
+++ UI/Tabs/EggsTab.lua
@@ -1,1 +1,25 @@
+-- Tabs/eggs.lua
+local EggsTab = {}
 
+function EggsTab.Init(Window, flags)
+    local tab = Window:MakeTab({Name = "Eggs", Icon = "rbxassetid://6023426915"})
+
+    tab:AddToggle({
+        Name = "Auto Egg (Abrir Rápido / Skip)",
+        Default = false,
+        Callback = function(Value)
+            flags.AutoEgg = Value
+        end
+    })
+
+    tab:AddDropdown({
+        Name = "Quantidade de Abertura",
+        Default = 1,
+        Values = {1, 2},
+        Callback = function(Value)
+            flags.EggAmount = tonumber(Value)
+        end
+    })
+end
+
+return EggsTab
