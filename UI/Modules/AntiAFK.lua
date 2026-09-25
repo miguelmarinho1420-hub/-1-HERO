3ec06c98f17ce198430d9af8a5f7aa3615c4cb71
miguelmarinho1420-hub <miguel.marinho1420@gmail.com>
2026-09-25 09:25:24 -0300

    Update AntiAFK.lua

--- UI/Modules/AntiAFK.lua
+++ UI/Modules/AntiAFK.lua
@@ -1,1 +1,11 @@
+local AntiAFK = {}
 
+local VirtualUser = game:GetService("VirtualUser")
+
+function AntiAFK.Execute()
+    -- Simula um clique de botão do mouse via serviço virtual para resetar o contador de AFK do jogo
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new(0, 0))
+end
+
+return AntiAFK
