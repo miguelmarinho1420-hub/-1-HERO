9f24cbb4bf77d603ce523cbc7be6784a800916ba
miguelmarinho1420-hub <miguel.marinho1420@gmail.com>
2026-09-25 10:19:20 -0300

    Update EventsTab.lua

--- UI/Tabs/EventsTab.lua
+++ UI/Tabs/EventsTab.lua
@@ -1,1 +1,32 @@
+-- Tabs/events.lua
+local EventsTab = {}
 
+function EventsTab.Init(Window, flags)
+    local tab = Window:MakeTab({Name = "Events", Icon = "rbxassetid://6023426915"})
+
+    tab:AddToggle({
+        Name = "Auto Dungeon (Bypass Parkour)",
+        Default = false,
+        Callback = function(Value)
+            flags.AutoDungeon = Value
+        end
+    })
+
+    tab:AddToggle({
+        Name = "Auto Boss (Aceitar & Desviar)",
+        Default = false,
+        Callback = function(Value)
+            flags.AutoBoss = Value
+        end
+    })
+
+    tab:AddToggle({
+        Name = "Endless Coop",
+        Default = false,
+        Callback = function(Value)
+            flags.AutoEndless = Value
+        end
+    })
+end
+
+return EventsTab
