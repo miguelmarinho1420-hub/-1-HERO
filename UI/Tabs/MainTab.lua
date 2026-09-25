2887a8ce24ba50eee57bbee52e133d41ff73550d
miguelmarinho1420-hub <miguel.marinho1420@gmail.com>
2026-09-25 10:18:49 -0300

    Update MainTab.lua

--- UI/Tabs/MainTab.lua
+++ UI/Tabs/MainTab.lua
@@ -1,1 +1,24 @@
+-- Tabs/main.lua
+local MainTab = {}
 
+function MainTab.Init(Window, flags)
+    local tab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://6023426915"})
+
+    tab:AddToggle({
+        Name = "Auto Click",
+        Default = false,
+        Callback = function(Value)
+            flags.AutoClick = Value
+        end
+    })
+
+    tab:AddToggle({
+        Name = "Auto Rebirth",
+        Default = false,
+        Callback = function(Value)
+            flags.AutoRebirth = Value
+        end
+    })
+end
+
+return MainTab
